8d0c8cbf58d574373abc4d24adf4cc8d96e6565b
yuxintig <71702790+yuxintig@users.noreply.github.com>
2020-10-04 22:03:20 +0800

    Add files via upload

---
+++ day4.java
@@ -0,0 +1,12 @@
+package GitDay;
+
+public class day4 {
+    public static void main(String[] args) {
+        int sum = 0,i = 0,j=0;
+        for(i = 1; i<=100;i++);
+        {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
