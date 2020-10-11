11bd3d69984d4dd1ebe57c25be5646d34da355c1
yuxintig <71702790+yuxintig@users.noreply.github.com>
2020-10-11 18:33:26 +0800

    Add files via upload

---
+++ day6.java
@@ -0,0 +1,24 @@
+package git;
+
+/**
+ * @ClassName day6
+ * @Description TODO
+ * @Author 余欣婷
+ * @Date 2020/10/11
+ **/
+public class day6 {
+    public static void main(String[] args) {
+        byte b = 22;
+        int n = 129;
+        float f = 123456.6789f;
+        double d = 123456789.123456789;
+        System.out.println("b = "+b);
+        System.out.println("n = "+n);
+        System.out.println("f = "+f);
+        System.out.println("d = "+d);
+        b = (byte)n;
+        f = (float)d;
+        System.out.println("b = "+b);
+        System.out.println("f = "+f);
+    }
+}
