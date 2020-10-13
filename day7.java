f501ffd21806c157933d7e1690d6059e62a31214
yuxintig <71702790+yuxintig@users.noreply.github.com>
2020-10-13 22:10:11 +0800

    Add files via upload

---
+++ day7.java
@@ -0,0 +1,19 @@
+package git;
+
+import java.util.Scanner;
+
+public class day7 {
+    public static void main(String[] args) {
+        System.out.println("用空格做分隔，输入若干个数，最后输入#结束，\n然后回车确认.");
+        Scanner reader = new Scanner(System.in);
+        double sum = 0;
+        int m = 0;
+        while(reader.hasNextDouble()){
+            double x = reader.nextDouble();
+            m = m+1;
+            sum = sum+x;
+        }
+        System.out.println(m+"个数和为"+sum);
+        System.out.println(m+"个数平均值"+sum/m);
+    }
+}
