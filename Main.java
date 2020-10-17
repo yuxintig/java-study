42cc369ab1d86be9a27659dda83d6c25127e0abb
yuxintig <71702790+yuxintig@users.noreply.github.com>
2020-10-17 21:52:02 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package org.yxt.learn.week1;
+
+/**
+ * @ClassName Main
+ * @Description TODO
+ * @Author YXT
+ * @Date 10/5/2020
+ **/
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
