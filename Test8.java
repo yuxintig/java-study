6858bff38ac2ba79ff62a76d32cd4ecdb5695dda
yuxintig <71702790+yuxintig@users.noreply.github.com>
2020-10-31 22:22:05 +0800

    Add files via upload

---
+++ Test8.java
@@ -0,0 +1,28 @@
+package panzeng.day09;
+
+/**
+ * @ClassName Test8
+ * @Description TODO
+ * @Author 南风有你
+ * @Date 2020/10/28
+ **/
+public class Test8 {
+    public static void main(String[] args) {
+        byte b1 = 10;
+        byte b2 = 20;
+        int b3 = b1 + b2;
+        System.out.println("byte类型b1和b2的和为:");
+        System.out.println(b3);
+        short s1 = 1000;
+        short s2 = 2000;
+        int s3 = s1 + s2;
+        System.out.println("byte类型s1和s2的和为:");
+        System.out.println(s3);
+        char ch1 = 'a';
+        int i1 = 30;
+        int ch3 = ch1 -i1;
+        System.out.println("char类型ch1和int类型i1的差为:");
+        System.out.println(ch3);
+
+    }
+}
